002eb834741d0178194522f7bb5e553a7ee63e9a
Kihare1 <victorfmuniz@hotmail.com>
2023-06-14 15:51:53 -0300

    with randomizer and js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 const holes = document.querySelectorAll(".hole")
-// let randomNumber = Math.floor(Math.random() * holes.length)
-// console.log(holes[randomNumber])
-// holes[randomNumber].classList.add("animationx")
 let count = 0
 function randomHole() {
   let randomNumber = Math.floor(Math.random() * holes.length)
